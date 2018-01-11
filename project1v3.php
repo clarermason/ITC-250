dc9c376a397a59e60bd5181be9a16e585e5b484b
BealBC <benjaminbeal@Benjamins-MacBook-Pro.local>
2018-01-11 15:45:38 -0800

    Final Commit
    My final commit includes CSS, PHP, and Background image.

--- project1v3.php
+++ project1v3.php
@@ -1,28 +1,53 @@
+<!doctype html>
+<html>
+ <head>
+     <meta charset="utf-8" />
+     <meta name="robots" content="noindex,nofollow" />
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link rel="stylesheet" href="styles.css">
+     <title>Temperature Conversion</title>
+</head>
+<body>
+  <div class="tc">
 <?php
 // show the form
   echo '<center>
 <h1> Temperature Converter </h1>
 <br>
-<form action="" method ="post">
-<input name="num1" type="text"  >
-  <select name="var1">
-  	<option value="" disabled selected>Select your option</option>
-    <option value="kelvin">Kelvin</option>
-    <option value="celsius">Celsius</option>
-    <option value="fahrenheit">Fahrenheit</option>
-  </select>
- to
- <select name="var2">
- 	<option value="" disabled selected>Select your option</option>
-   	<option value="kelvin">Kelvin</option>
-   	<option value="celsius">Celsius</option>
-   	<option value="fahrenheit">Fahrenheit</option>
- </select>
-  <br><br>
-  <input type="submit">
+<form class="tempConversion" action="" method ="post">
+
+<input name="num1" type="text" size="27" placeholder="Enter Temperature Value" > 
+		
+<br/>
+
+From: 
+
+	<select name="var1">
+		<option value="" disabled selected>Select your option</option>
+		<option value="kelvin">Kelvin</option>
+		<option value="celsius">Celsius</option>
+		<option value="fahrenheit">Fahrenheit</option>
+  	</select>
+<br/>
+To:
+
+	 <select name="var2">
+		<option value="" disabled selected>Select your option</option>
+		<option value="kelvin">Kelvin</option>
+		<option value="celsius">Celsius</option>
+		<option value="fahrenheit">Fahrenheit</option>
+	 </select>
+
+<br/>
+<br/>
+<div class="submit">
+<input class="button" type="submit" value="Calculate">
+<input type="reset" name="resetButton" class="button" /> 
+</div>
 </form>
   ';
-	//*if there is userinput, show the answer
+
+//*if there is userinput, show the answer
 if(isset($_POST['var1'])){
 $type1 = $_POST['var1'];
 $type2 = $_POST['var2'];
@@ -62,3 +87,7 @@ else if($type1 == "fahrenheit" && $type2 == "fahrenheit"){
 }
 echo '</center>';
 }
+?>
+</div>
+</body>
+</html>
